3a0699d6ac2ef60339f02af3d6da05bd4c26d4cc
Timo van 't Wout <60403844+timovantwout@users.noreply.github.com>
2020-11-10 13:35:40 +0100

    Toevoegen winkelmandje.php en logo

---
+++ winkelmandje.php
@@ -0,0 +1,31 @@
+<?php include __DIR__ . "/header.php"; ?>
+<div class="winkelmandje">
+    <h1>Winkelmandje</h1>
+    <div class="overzicht">
+        <div class="product-overzicht">
+            <table>
+                <tr>
+                    <th>Foto</th>
+                    <th>Title</th>
+                    <th>Prijs</th>
+                    <th>Aantal</th>
+                    <th>Subtotaal</th>
+                    <th>Verwijderen</th>
+                </tr>
+                <tr>
+                    <td><img></td>
+                    <td><p>Test</p></td>
+                    <td><p>€395</p></td>
+                    <td><p>2</p></td>
+                    <td><p>€790</p></td>
+                    <td>X</td>
+                </tr>
+            </table>
+        </div>
+        <div class="prijs-overzicht">
+        
+        </div>
+    </div>
+
+
+</div>
