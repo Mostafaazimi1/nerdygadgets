6342042bd6f5f3359f7fb267666b2f7f3c27ab2b
Mostafaazimi1 <46542565+Mostafaazimi1@users.noreply.github.com>
2020-11-19 11:38:31 +0100

    spatie

--- losseFunctions.php
+++ losseFunctions.php
@@ -1,28 +1,28 @@
 <?php
 if (!isset($_SESSION['winkelwagen'])) {
-$_SESSION['winkelwagen'] = array();
+    $_SESSION['winkelwagen'] = array();
 }
 $winkelwagen = $_SESSION['winkelwagen'];
 
 if (isset($_GET['delete'])) {
-$winkelwagen = deleteProduct($winkelwagen, (int)$_GET['delete']);
-header("Refresh:0; url=winkelmandje.php");
+    $winkelwagen = deleteProduct($winkelwagen, (int)$_GET['delete']);
+    header("Refresh:0; url=winkelmandje.php");
 }
 
 if (isset($_POST['Afreken_submit'])) {
-$_SESSION['AfrekenGegevens'] = array(
-'voornaam' => $_POST['voornaam'],
-'tussenv' => $_POST['tussenv'],
-'achternaam' => $_POST['achternaam'],
-'postcode' => $_POST['postcode'],
-'huisnummer' => $_POST['huisnummer'],
-'toev' => $_POST['toev'],
-'straat' => $_POST['straat'],
-'plaats' => $_POST['plaats'],
-'email' => $_POST['email'],
-'telefoonnummer' => $_POST['telefoonnummer']
-);
+    $_SESSION['AfrekenGegevens'] = array(
+        'voornaam' => $_POST['voornaam'],
+        'tussenv' => $_POST['tussenv'],
+        'achternaam' => $_POST['achternaam'],
+        'postcode' => $_POST['postcode'],
+        'huisnummer' => $_POST['huisnummer'],
+        'toev' => $_POST['toev'],
+        'straat' => $_POST['straat'],
+        'plaats' => $_POST['plaats'],
+        'email' => $_POST['email'],
+        'telefoonnummer' => $_POST['telefoonnummer']
+    );
 
-header("Location: overzicht.php");
-die();
+    header("Location: overzicht.php");
+    die();
 }
