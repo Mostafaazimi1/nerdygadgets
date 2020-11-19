cbd5c7ab3d231477c40d2589d62dc43c8d853a19
jjdrost <43378390+jjdrost@users.noreply.github.com>
2020-11-19 23:53:11 +0100

    ORDERS

--- overzicht.php
+++ overzicht.php
@@ -6,6 +6,47 @@ $products = loadProducts($winkelwagen, $Connection);
 $afrekenGegevens = $_SESSION['AfrekenGegevens'];
 
 ?>
+
+<?php
+if(isset($_GET['afronden'])){
+
+        $winkelwagen = $_SESSION['winkelwagen'];
+        $products = loadProducts($winkelwagen, $Connection);
+        $allTotal = 0;
+        $salesID = 1;
+        $contactID = 1;
+        $orderDate = date("Y/m/d");
+        $deliveryDate = date("Y/m/d");
+        $backOrderI = 1;
+        $lastEditBY = 1;
+        $lastEditDate = date("Y/m/d");
+
+        $allTotal = 0;
+        foreach ($products as $product) {
+
+            $gegevens = $_SESSION['login'];
+
+            $productUpdate = mysqli_prepare($Connection, "UPDATE stockitemholdings SET QuantityOnHand = QuantityOnHand-(?) WHERE StockItemID=(?)");
+            mysqli_stmt_bind_param($productUpdate, 'ii', $product['aantal'], $product['id']);
+            mysqli_stmt_execute($productUpdate);
+
+            $orderInput = mysqli_prepare($Connection, "INSERT INTO orders (CustomerID, SalespersonPersonID, ContactPersonID, OrderDate, 
+                                                    ExpectedDeliveryDate, IsUndersupplyBackordered, LastEditedBy, LastEditedWhen) VALUES(?,?,?,?,?,?,?,?)");
+            mysqli_stmt_bind_param($orderInput, 'iiissiis', $gegevens['CustomerID'], $salesID, $contactID, $orderDate, $deliveryDate, $backOrderI, $lastEditBY, $lastEditDate);
+            mysqli_stmt_execute($orderInput);
+            //return mysqli_stmt_affected_rows($orderInput) == 1;
+        }
+        $_SESSION['messageCount2'] = 1;
+        print('<meta http-equiv = "refresh" content = "0; url = ./" />');
+}
+else{
+    print("else1");
+}
+
+?>
+
+
+
 <?php
 if (isset($_SESSION["AfrekenGegevens"])) {
     ?>
