66335cda443466efc816bf855c7b87f1505cdd99
Cindy012 <74193681+Cindy012@users.noreply.github.com>
2020-11-30 14:41:47 +0100

    mail.php vereenvoudigd

--- klantenservice.php
+++ klantenservice.php
@@ -7,14 +7,19 @@ if (isset($_SESSION["login"])) {
     $FullName = $gegevens['FullName'];
     $email = $gegevens['EmailAddress'];
     $HouseNumber = $gegevens['PhoneNumber'];
+} else{
+    $FullName = "";
+    $email = "";
+    $HouseNumber = "";
 }
 ?>
 <h2>Contact</h2><br>
 <p>Bij vragen en opmerkingen kunt u gerust met ons klantenservice contact opnemen.</p>
-    U kunt ons dagelijks bereiken van 9:00-17:00.<br>
-    Telefoonnummer: <a href="tel:0612345678">0612345678</a><br>
+
+U kunt ons dagelijks telefonisch bereiken van 9:00-17:00.<br>
+Telefoonnummer: <a href="tel:0612345678">0612345678</a><br>
 <br>
-U kunt ons ook mailen als bellen voor u niet uitkomt.<br>
+U kunt ons altijd mailen. (Onze klantenservice reageert zo snel mogelijk op uw vraag)<br>
 Mail: <a href="mailto:klantenservice.nerdygadgets@gmail.com">klantenservice.nerdygadgets@gmail.com</a><br>
 <br>
 <form action="mail.php" method="post">
@@ -25,3 +30,9 @@ Mail: <a href="mailto:klantenservice.nerdygadgets@gmail.com">klantenservice.nerd
     Vraag: <br><textarea type="text" name="tekst" placeholder="Stel hier uw vraag of klacht" required></textarea><br>
     <button type="submit" name="sendmail">Verzend</button>
 </form>
+
+<!--Niet zeker of dit wordt geimplementeerd...-->
+<!--<br><br>-->
+<!--<h2>Vragen</h2>-->
+<!--<h5>Is het normaal dat ik nog steeds geen reactie van de klantenservice heb gekregen sinds 2 dagen nadat ik het contactformulier/email heb verzonden?</h5>-->
+<!--<p>Onze klantenservice probeert altijd zo snel mogelijk uw vraag te beantwoorden. Meestal duurt het maximaal 1 à 2 dagen voordat u reactie krijgt, in uiterste gevalllen wanneer het heel druk is kan het tot 5 dagen duren. Vind u dat het heel dringend is? Dan kunt u proberen telefonisch contact opnemen met de klantenservice</p>-->
--- mail.php
+++ mail.php
@@ -7,17 +7,13 @@ if(isset($_POST["sendmail"])) {
    $tekst = $_POST["tekst"];
    $to = "klantenservice.nerdygadgets@gmail.com";
 
-   if (!$tekst) {
-       print("Vul uw vraag of klacht in! <br> <a href='klantenservice.php'>Ga terug naar het contactformulier...</a>");
+   if ($telNummer) {
+       $volledigetekst = $tekst . "\nVan " . $naam . "\n$email" . "\ntelefoonnummer: " . $telNummer;
+       $mailen = mail($to, $onderwerp, $volledigetekst);
+       header("Location: klantenservice.php?bericht&verzenden=succes");
    } else {
-       if ($telNummer) {
-           $volledigetekst = $tekst . "\nVan " . $naam . "\n$email" . "\ntelefoonnummer: " . $telNummer;
-           $mailen = mail($to, $onderwerp, $volledigetekst);
-           header("Location: klantenservice.php?bericht&verzenden=succes");
-       } else {
-           $volledigetekst = $tekst . "\nVan " . $naam . "\n$email";
-           $mailen = mail($to, $onderwerp, $volledigetekst);
-           header("Location: klantenservice.php?bericht&verzenden=succes");
-       }
+       $volledigetekst = $tekst . "\nVan " . $naam . "\n$email";
+       $mailen = mail($to, $onderwerp, $volledigetekst);
+       header("Location: klantenservice.php?bericht&verzenden=succes");
    }
 }
