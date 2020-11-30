5c6d32d05c6d6b25ea4483d68f32e93f0c0dad5c
jjdrost <43378390+jjdrost@users.noreply.github.com>
2020-11-30 05:09:08 +0100

    Guest User login
    Guest krijgt permited to logon 0 en pass "NOT SET" mee,
    Als gebruiker wachtwoord verkeerd heeft moet het een melding krijgen :) data blijft in het form

--- Afrekenen.php
+++ Afrekenen.php
@@ -1,76 +1,104 @@
 <?php
 include __DIR__ . "/header.php";
 
-// Code from header
+$winkelwagen = $_SESSION['winkelwagen'];
+$products = loadProducts($winkelwagen, $Connection);
+$complete=false;
+$createAccount=false;
+$createGuest=false;
+$wrongPass=false;
+
+//Start variable
+if (!isset($_POST['Afreken_submit'])) {
+    $FirstName = "";
+    $LastName = "";
+    $postcode = "";
+    $HouseNumber = "";
+    $StreetName ="";
+    $Plaats = "";
+    $email = "";
+    $PhoneNumber = "";
+}
+
+//Set variable in FORM when logon is set
+if (isset($_SESSION["login"])) {
+    // Gebruiker is ingelogd
+    $gegevens = $_SESSION['login'];
+    $HuisnummerStraat = (explode(" ", $gegevens['DeliveryAddressLine2'], 2));
+    $FirstName = $gegevens["PreferredName"];
+    $LastName = (str_replace($gegevens['PreferredName'] . " ", "", $gegevens['FullName']));
+    $postcode = $gegevens['DeliveryPostalCode'];
+    $HouseNumber = $HuisnummerStraat[0];
+    $StreetName = $HuisnummerStraat[1];
+    $Plaats = $gegevens['CityName'];
+    $email = $gegevens['EmailAddress'];
+    $PhoneNumber = $gegevens['PhoneNumber'];
+}
+?>
+
+<?php
 if (isset($_POST['Afreken_submit'])) {
     $_SESSION['AfrekenGegevens'] = array(
         'voornaam' => $_POST['FirstName'],
         'achternaam' => $_POST['LastName'],
         'postcode' => $_POST['postcode'],
         'huisnummer' => $_POST['HouseNumber'],
-//        'toev' => $_POST['toev'],
         'straat' => $_POST['StreetName'],
         'plaats' => $_POST['Plaats'],
         'email' => $_POST['email'],
         'telefoonnummer' => $_POST['PhoneNumber']
     );
-
-    if (isset($_POST['account_aanmaken'])) {
-        $_SESSION['AfrekenGegevens']['newAcc'] = TRUE;
-        $_SESSION['AfrekenGegevens']['password'] = $_POST['password'];
-    }
-
-//    header("Location: overzicht.php");
-    print('<meta http-equiv = "refresh" content = "0; url = ./overzicht.php" />');
-//    die();
 }
 
-$winkelwagen = $_SESSION['winkelwagen'];
-$products = loadProducts($winkelwagen, $Connection);
-
-// gegevens van de niet ingelogde klant worden bewaard in de database table customers
-// eerst opvragen customerid die hoort bij session -- customerid is gelinkt met customerid van table orders
 
 
-// Gebruiker is niet ingelogd
-        $FirstName = "";
-        $tussenvoegsel = "";
-        $LastName = "";
-        $postcode = "";
-        $HouseNumber = "";
-//        $toev = "";
-        $StreetName = "";
-        $Plaats = "";
-        $email = "";
-        $PhoneNumber = "";
+if (isset($_SESSION["login"]) AND isset($_POST['Afreken_submit'])) {
+    $complete=TRUE;
+}
 
-if(isset($_POST["Afreken_submit"])) {
-    if (isset($_SESSION["login"])) {
-        // Gebruiker is ingelogd
-        $gegevens = $_SESSION['login'];
-        $HuisnummerStraat = (explode(" ", $gegevens['DeliveryAddressLine2']));
-        $FirstName = $gegevens["PreferredName"];
-        $LastName = (str_replace($gegevens['PreferredName'] . " ", "", $gegevens['FullName']));
-        $postcode = $gegevens['DeliveryPostalCode'];
-        $HouseNumber = $HuisnummerStraat[0];
-//        $toev = "";
-        $StreetName = $HuisnummerStraat[1];
-        $Plaats = $gegevens['PostalAddressLine2'];
-        $email = $gegevens['EmailAddress'];
-        $HouseNumber = $gegevens['PhoneNumber'];
-        $Plaats = ucfirst($_POST["Plaats"]);
+if (!isset($_SESSION["login"]) AND isset($_POST['Afreken_submit'])) {
+    if (isset($_POST['account_aanmaken'])) {
+        if (($_POST["password"]) == ($_POST["confirmpassword"])) {
+            $createAccount = TRUE;
+        } else {
+            echo("pass don't match");
+            $FirstName=$_POST['FirstName'];
+            $LastName=$_POST['LastName'];
+            $postcode=$_POST['postcode'];
+            $HouseNumber=$_POST['HouseNumber'];
+            $StreetName=$_POST['StreetName'];
+            $Plaats=$_POST['Plaats'];
+            $email=$_POST['email'];
+            $PhoneNumber=$_POST['PhoneNumber'];
+        }
     }
-    //KOMT $plaats VOOR IN COLUMN CITYNAME VAN TABEL CITIES ZO JA RETURN COLUMN VALUE VAN CITYID EN GEEF DEZE AAN $DeliveryCityId
-    // ANDERS AFBREKEN
-    $sql = "SELECT CityName FROM cities WHERE CityName = '" . $Plaats . "' LIMIT 1";
-    $result = $Connection->query($sql);
-    $aantalresult = mysqli_num_rows($result);
-    if ($aantalresult < 1) {
-        echo("Sorry, in " . $Plaats . " leveren wij niet, voer alsjeblieft een nieuw adres in.");
-        // MOET ERBIJ: GEGEVENS OPSLAAN BIJ FOUT
-    } else {
+    elseif(!isset($_POST['account_aanmaken'])){
+        $createGuest=TRUE;
+    }
+
+    if ($createAccount == TRUE or $createGuest == TRUE) {
+        $Plaats = ucfirst($_POST["Plaats"]);
+        $sql = "
+                    SELECT CityName
+                    FROM cities
+                    WHERE CityName = '" . $Plaats . "'
+                    LIMIT 1";
+        $result = $Connection->query($sql);
+        $aantalresult = mysqli_num_rows($result);
+        if ($aantalresult < 1) {
+            echo("Sorry, in " . $Plaats . " leveren wij niet, voer alsjeblieft een nieuw adres in.");
+        } else {
+            if ($createAccount == true) {
+                $password = $_POST["password"];
+                $IsPermittedToLogon = 1;
+            } else {
+                $password = "NOT SET";
+                $IsPermittedToLogon = 0;
+                echo("pass NOT SET");
+
+            }
+        }
         $DeliveryCityName = $Plaats;
-        $password = $_POST["password"];
         $email = $_POST["email"];
         $PhoneNumber = $_POST["PhoneNumber"];
         $postcode = $_POST["postcode"];
@@ -96,7 +124,6 @@ if(isset($_POST["Afreken_submit"])) {
         $BillToCustomerId = 1;
         $DeliveryCityId = 1;
         $LastEditedBy = 1;
-        $IsPermittedToLogon = 1;
         $IsExternalLogonProvider = 1;
         $IsSystemUser = 0;
         $IsEmployee = 0;
@@ -118,161 +145,37 @@ if(isset($_POST["Afreken_submit"])) {
             $postcode, $unknown, $unknown, $DeliveryCityName, $postcode, $LastEditedBy, $CurrentDate,
             $ValidTo);
         $execval = $stmt->execute();
-        echo $execval;
-        //echo "Customer gegevens zijn succesvol toegevoegd aan database!";
         $stmt->close();
-    }
 
-    // VRAAG VALUE VAN CUSTOMERID UIT CUSTOMERS EN GEEF DEZE EIGEN VARIABELEN -zodat je ze in people tabel kan inserten!
-    $sql = "SELECT CustomerID FROM customers WHERE CustomerName = ('$FullName') AND DeliveryPostalCode =
-            ('$postcode') AND DeliveryAddressLine2 = ('$address')";
-    $result = $Connection->query($sql);
-    $row = mysqli_fetch_array($result);
-    $CustomerNUM = reset($row);
-
-    //      Als verbinding gesloten is, wordt de SQL query voorbereid.
-    if (isset($_POST["account_aanmaken"]) and isset($_POST["password"]) and isset($_POST["confirmpassword"])) {
-        if (($_POST['account_aanmaken'] == 'ja') and (($_POST["password"]) != ($_POST["confirmpassword"]))) {
-            // GEGEVENS IN PEOPLE image(Photo) = blob
-            $stmt1 = $Connection->prepare("insert into people(FullName, PreferredName, SearchName, IsPermittedToLogon, LogonName,
-                            IsExternalLogonProvider, HashedPassword, IsSystemUser, IsEmployee, IsSalesPerson,
-                            UserPreferences, PhoneNumber, FaxNumber, EmailAddress, Photo, CustomFields,
-                            OtherLanguages, LastEditedBy, ValidFrom, ValidTo, CustomerNUM)
-                            values(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
-            $stmt1->bind_param("sssisisiiissssbssisss", $FullName, $FirstName, $FullName, $IsPermittedToLogon, $email,
-                $IsExternalLogonProvider, $password, $IsSystemUser, $IsEmployee, $IsSalesPerson, $empty,
-                $PhoneNumber, $empty, $email, $empty, $empty, $empty, $LastEditedBy, $CurrentDate, $ValidTo, $CustomerNUM);
-            $execval = $stmt1->execute();
-            echo $execval;
-            //echo "People gegevens zijn succesvol toegevoegd aan database!";
-            $stmt1->close();
-        } else {
-            // De wachtwoorden moeten overeenkomen! Maak hier code zodat gebruiker terug wordt verwezen en value gevult wordt
-        }
-    }
-    // UPDATEN BILLTOCUSTOMERID IN TABLE customers
-    $query = "UPDATE customers SET BillToCustomerID = ? WHERE CustomerID = ?";
-    $Statement = mysqli_prepare($Connection, $query);
-    mysqli_stmt_bind_param($Statement, "ii", $CustomerNUM, $CustomerNUM);
-    mysqli_stmt_execute($Statement);
-    if (isset($_SESSION["login"])) {
-        // Gebruiker is ingelogd
-        $gegevens = $_SESSION['login'];
-        $HuisnummerStraat = (explode(" ", $gegevens['DeliveryAddressLine2'], 2));
-        $FirstName = $gegevens["PreferredName"];
-        $LastName = (str_replace($gegevens['PreferredName'] . " ", "", $gegevens['FullName']));
-        $postcode = $gegevens['DeliveryPostalCode'];
-        $HouseNumber = $HuisnummerStraat[0];
-        $tussenvoegsel = "";
-        $StreetName = $HuisnummerStraat[1];
-        $Plaats = $gegevens['CityName'];
-        $email = $gegevens['EmailAddress'];
-        $PhoneNumber = $gegevens['PhoneNumber'];
-    } else {
-        // Gebruiker is niet ingelogd
-        $tussenvoegsel = "";
-        $Plaats = ucfirst($_POST["Plaats"]);
-    }
-
-    if (isset($_POST["Afreken_submit"])) {
-        //KOMT $plaats VOOR IN COLUMN CITYNAME VAN TABEL CITIES ZO JA RETURN COLUMN VALUE VAN CITYID EN GEEF DEZE AAN $DeliveryCityId
-        // ANDERS AFBREKEN
-        $sql = "SELECT CityName FROM cities WHERE CityName = '" . $Plaats . "' LIMIT 1";
-        $result = $Connection->query($sql);
-        $aantalresult = mysqli_num_rows($result);
-        if ($aantalresult < 1) {
-            echo("Sorry, in " . $Plaats . " leveren wij niet, voer alsjeblieft een nieuw adres in.");
-            // MOET ERBIJ: GEGEVENS OPSLAAN BIJ FOUT
-        } else {
-            $DeliveryCityName = $Plaats;
-            $password = $_POST["password"];
-            $email = $_POST["email"];
-            $PhoneNumber = $_POST["PhoneNumber"];
-            $postcode = $_POST["postcode"];
-            $FirstName = $_POST["FirstName"];
-            $LastName = $_POST["LastName"];
-            $HouseNumber = $_POST["HouseNumber"];
-            $StreetName = $_POST["StreetName"];
-            $CurrentDate = date("Y/m/d");
-            $FullName = ($FirstName . " " . $LastName);
-            $BuyingGroupId = 1;
-            $PrimaryContactPersonId = 9;
-            $AlternateContactPersonId = 9;
-            $CustomerCategoryId = 1;
-            $CreditLimit = 0.00;
-            $StandardDiscountPercentage = 0.000;
-            $zero = 0;
-            $seven = 7;
-            $three = 3;
-            $NULL = NULL;
-            $unknown = "unknown";
-            $address = ($HouseNumber . " " . $StreetName);
-            $ValidTo = "9999-12-31";
-            $BillToCustomerId = 1;
-            $DeliveryCityId = 1;
-            $LastEditedBy = 1;
-            $IsPermittedToLogon = 1;
-            $IsExternalLogonProvider = 1;
-            $IsSystemUser = 0;
-            $IsEmployee = 0;
-            $IsSalesPerson = 0;
-            $empty = "";
-            //Als verbinding gesloten is, wordt de SQL query voorbereid.
-            // GEGEVENS IN CUSTOMERS                    faxnumber = string
-            $stmt = $Connection->prepare(
-                "INSERT INTO customers (CustomerName, BillToCustomerID, CustomerCategoryID, BuyingGroupID, PrimaryContactPersonID,
-                                AlternateContactPersonID, DeliveryMethodID, DeliveryCityID, PostalCityID, CreditLimit,
-                                AccountOpenedDate, StandardDiscountPercentage, IsStatementSent, IsOnCreditHold, PaymentDays,
-                                PhoneNumber, FaxNumber, DeliveryRun, RunPosition, WebsiteURL, DeliveryAddressLine1,
-                                DeliveryAddressLine2, DeliveryPostalCode, DeliveryLocation, PostalAddressLine1, PostalAddressLine2,
-                                PostalPostalCode, LastEditedBy, ValidFrom, ValidTo)
-                                VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
-            $stmt->bind_param("siiiiiiiidsdiiissssssssssssiss", $FullName, $BillToCustomerId, $CustomerCategoryId, $BuyingGroupId, $PrimaryContactPersonId,
-                $AlternateContactPersonId, $three, $DeliveryCityId, $DeliveryCityId, $CreditLimit, $CurrentDate, $StandardDiscountPercentage, $zero, $zero, $seven,
-                $PhoneNumber, $zero, $NULL, $NULL, $zero, $unknown, $address,
-                $postcode, $unknown, $unknown, $DeliveryCityName, $postcode, $LastEditedBy, $CurrentDate,
-                $ValidTo);
-            $execval = $stmt->execute();
-            echo $execval;
-            //echo "Customer gegevens zijn succesvol toegevoegd aan database!";
-            $stmt->close();
-        }
-        //      Als verbinding gesloten is, wordt de SQL query voorbereid.
-        if (isset($_POST["account_aanmaken"]) and isset($_POST["password"]) and isset($_POST["confirmpassword"])) {
-            if (($_POST['account_aanmaken'] == 'ja') and (($_POST["password"]) != ($_POST["confirmpassword"]))) {
-                // VRAAG VALUE VAN CUSTOMERID UIT CUSTOMERS EN GEEF DEZE EIGEN VARIABELEN -zodat je ze in people tabel kan inserten!
-                $sql = "SELECT CustomerID FROM customers WHERE CustomerName = ('$FullName') AND DeliveryPostalCode =
+        $sql = "SELECT CustomerID FROM customers WHERE CustomerName = ('$FullName') AND DeliveryPostalCode =
                     ('$postcode') AND DeliveryAddressLine2 = ('$address')";
-                $result = $Connection->query($sql);
-                $row = mysqli_fetch_array($result);
-                $CustomerNUM = reset($row);
+        $result = $Connection->query($sql);
+        $row = mysqli_fetch_array($result);
+        $CustomerNUM = reset($row);
 
-                // GEGEVENS IN PEOPLE image(Photo) = blob
-                $stmt1 = $Connection->prepare("insert into people(FullName, PreferredName, SearchName, IsPermittedToLogon, LogonName,
+        // GEGEVENS IN PEOPLE image(Photo) = blob
+        $stmt1 = $Connection->prepare("insert into people(FullName, PreferredName, SearchName, IsPermittedToLogon, LogonName,
                             IsExternalLogonProvider, HashedPassword, IsSystemUser, IsEmployee, IsSalesPerson,
                             UserPreferences, PhoneNumber, FaxNumber, EmailAddress, Photo, CustomFields,
                             OtherLanguages, LastEditedBy, ValidFrom, ValidTo, CustomerNUM)
                             values(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
-                $stmt1->bind_param("sssisisiiissssbssisss", $FullName, $FirstName, $FullName, $IsPermittedToLogon, $email,
-                    $IsExternalLogonProvider, $password, $IsSystemUser, $IsEmployee, $IsSalesPerson, $empty,
-                    $PhoneNumber, $empty, $email, $empty, $empty, $empty, $LastEditedBy, $CurrentDate, $ValidTo, $CustomerNUM);
-                $execval = $stmt1->execute();
-                echo $execval;
-                //echo "People gegevens zijn succesvol toegevoegd aan database!";
-                $stmt1->close();
-            } else {
-                // De wachtwoorden moeten overeenkomen! Maak hier code zodat gebruiker terug wordt verwezen en value gevult wordt
-            }
-        }
-        // UPDATEN BILLTOCUSTOMERID IN TABLE customers
-        $query = "UPDATE customers SET BillToCustomerID = ? WHERE CustomerID = ?";
-        $Statement = mysqli_prepare($Connection, $query);
-        mysqli_stmt_bind_param($Statement, "ii", $CustomerNUM, $CustomerNUM);
-        mysqli_stmt_execute($Statement);
+        $stmt1->bind_param("sssisisiiissssbssisss", $FullName, $FirstName, $FullName, $IsPermittedToLogon, $email,
+            $IsExternalLogonProvider, $password, $IsSystemUser, $IsEmployee, $IsSalesPerson, $empty,
+            $PhoneNumber, $empty, $email, $empty, $empty, $empty, $LastEditedBy, $CurrentDate, $ValidTo, $CustomerNUM);
+        $execval = $stmt1->execute();
+        $stmt1->close();
+        $complete=TRUE;
     }
 }
 
+
+
+//$complete will be set true when $_sesion[login]isset OR Guest / new Customer is created
+if($complete){
+    echo('<meta http-equiv = "refresh" content = "0; url = ./overzicht.php" />');
+}
 ?>
+
 <form method="post" action="afrekenen.php">
     <div class="Bestelgegevens">
         <h1>Bestelgegevens</h1>
@@ -328,16 +231,16 @@ if(isset($_POST["Afreken_submit"])) {
                         </tr>
                         <?php if(!isset($_SESSION["login"])) {
                             echo "<div>";
-                                echo "<tr>";
-                                    echo "<th></th>";
-                                    echo "<td><input type='checkbox' name='account_aanmaken' value='ja'></td>";
-                                    echo "<td>account aanmaken</td>";
-                                echo "</tr>";
-                                echo "<tr>";
-                                    echo "<th></th>";
-                                    echo "<td><input type='password' placeholder='Wachtwoord' name='password' autocomplete='new-password'></td>";
-                                    echo "<td><input type='password' placeholder='Bevestig wachtwoord' name='confirmpassword' autocomplete='new-password'></td>";
-                                echo "</tr>";
+                            echo "<tr>";
+                            echo "<th></th>";
+                            echo "<td><input type='checkbox' name='account_aanmaken' value='ja'></td>";
+                            echo "<td>account aanmaken</td>";
+                            echo "</tr>";
+                            echo "<tr>";
+                            echo "<th></th>";
+                            echo "<td><input type='password' placeholder='Wachtwoord' name='password' autocomplete='new-password'></td>";
+                            echo "<td><input type='password' placeholder='Bevestig wachtwoord' name='confirmpassword' autocomplete='new-password'></td>";
+                            echo "</tr>";
                             echo "</div>";
                         }
                         ?>
@@ -390,3 +293,5 @@ if(isset($_POST["Afreken_submit"])) {
         </div>
     </div>
 </form>
+
+
