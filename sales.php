6645babcea82ed0f3ce357724df7ca5daad7161a
jjdrost <43378390+jjdrost@users.noreply.github.com>
2020-12-11 16:31:16 +0100

    com

--- sales.php
+++ sales.php
@@ -1,11 +1,22 @@
 <?php
+include __DIR__ . "/header.php";
 
 if (isset ($_SESSION['login'])) {
     $Medewerkerlogin = $_SESSION['login'];
     if ($Medewerkerlogin['IsSalesperson'] == 1) {
         //je bent gemachtigd om dit te zien!
+
+
+
+
+
     }
     else{
         //je bent NIET gemachtigd om dit te zien!
+        echo "<p style='font-size: 20px; margin-top: 24px;'>Helaas leuk geprobeert, maar U heeft GEEN toegang!</p>";
     }
+}
+else{
+    //je bent NIET gemachtigd om dit te zien!
+    echo "<p style='font-size: 20px; margin-top: 24px;'>Helaas leuk geprobeert, maar U heeft GEEN toegang!</p>";
 }
