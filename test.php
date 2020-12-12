2b26da43e8051bec2d36a3be9133e5ed68e4a365
Trezay <59511634+Trezay@users.noreply.github.com>
2020-12-12 15:27:54 +0100

    testing

--- test.php
+++ test.php
@@ -1,4 +1,4 @@
 <?php
-$Kaas = "Brood";
-$bevatHoofdletter = preg_match('/[A-Z]/', $Kaas);
-var_dump($bevatHoofdletter);
+$Kaas = "B";
+$bevatHoofdletter = preg_match('[A-Z]', $Kaas);
+print($bevatHoofdletter);
