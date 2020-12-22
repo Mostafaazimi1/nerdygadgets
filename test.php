75d5fbc18512205568eea2c235e62fe6bca27ba6
Cindy012 <74193681+Cindy012@users.noreply.github.com>
2020-12-22 22:44:52 +0100

    test.php in eigen vorm teruggebracht

--- test.php
+++ test.php
@@ -1,85 +1,15 @@
 <?php
-//include __DIR__ . "/header.php";
-//$FullName = "kaasje kaas";
-//$sql = "
-//                SELECT FullName
-//                FROM people
-//                WHERE FullName = '" . $FullName . "'";
-//$result = $Connection->query($sql);
-//$aantalresult = mysqli_num_rows($result);
-//if ($aantalresult < 1) {
-//    $validName = TRUE;
-//} else {
-//    echo("Sorry, de naam " . $FullName . " is al in gebruik.<br>");
-//}
-
-
-//$Medewerkerlogin = $_SESSION['login'];
-//if ($Medewerkerlogin['IsSalesperson'] == 1){
-//    $host = "localhost";
-//    $user = "medewerker";
-//    $password = "WO869N0a3ImlyC3Jsga";
-//    $database = "nerdygadgets";
-//
-//} elseif ($Medewerkerlogin['IsSalesperson'] == 0){
-//    $host = "localhost";
-//    $user = "geregistreerd";
-//    $password = "I3U7rpDAlwT7DGKh3eH";
-//    $database = "nerdygadgets";
-//
-//    $host = "localhost";
-//    $user = "geregistreerde_gebruiker";
-//    $password = "I3U7rpDAlwT7DGKh3eH";
-//    $database = "nerdygadgets";
-////}
-//    $host = "localhost";
-//    $user = "root";
-//    $password = "";
-//    $database = "nerdygadgets";
-//}
-
-
-if (isset($_SESSION['login']) == true){
-    print("ingelogd");
-    $Medewerkerlogin = $_SESSION['login'];
-    if ($Medewerkerlogin['IsSalesperson'] == 1){
-        $host = "localhost";
-        $user = "medewerker";
-        $password = "WO869N0a3ImlyC3Jsga";
-        $database = "nerdygadgets";
-    } elseif ($Medewerkerlogin['IsSalesperson'] == 0){
-        $host = "localhost";
-        $user = "geregistreerd";
-        $password = "I3U7rpDAlwT7DGKh3eH";
-        $database = "nerdygadgets";
-    }
-} else {
-    print("bezoeker");
-    $host = "localhost";
-    $user = "Bezoeker";
-    $password = "YO4vQRA3a8Kda4jb";
-    $database = "nerdygadgets";
-}
-mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT); // Set MySQLi to throw exceptions
-try {
-    $Connection = mysqli_connect($host, $user, $password, $database);
-    mysqli_set_charset($Connection, 'latin1');
-    $DatabaseAvailable = true;
-} catch (mysqli_sql_exception $e) {
-    $DatabaseAvailable = false;
-}
-if (!$DatabaseAvailable) {
-    ?><h2>Website wordt op dit moment onderhouden.</h2><?php
-    die();
-}
-print("<br>test<br>");
-$sql = "SELECT * FROM tickets";
+include __DIR__ . "/header.php";
+$FullName = "kaasje kaas";
+$sql = "
+                SELECT FullName
+                FROM people
+                WHERE FullName = '" . $FullName . "'";
 $result = $Connection->query($sql);
-mysqli_close($Connection);
-if ($result->num_rows > 0) {
-    while($row = $result->fetch_assoc()) {
-        print_r($row);
-    }
+$aantalresult = mysqli_num_rows($result);
+if ($aantalresult < 1) {
+    $validName = TRUE;
 } else {
-    print("geen rows");
+    echo("Sorry, de naam " . $FullName . " is al in gebruik.<br>");
 }
+
